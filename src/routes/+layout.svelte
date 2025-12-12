c22d429810876146de8d78a9b550bd1b2494581f
ArmutS <taha.mankir123@gmail.com>
2025-12-13 02:34:39 +0300

    Sync

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,21 @@
     import "../themes/dracula.css";
     import "../themes/gruvbox.css";
     import "../themes/carbonfox.css";
-    import "../themes/icons.css";
     import "../app.css";
     import { onMount } from "svelte";
-     
 
     function setTheme(themeName: string) {
         document.documentElement.setAttribute("data-theme", themeName);
     }
 
-
     onMount(() => {
-        setTheme("catppuccin-dark");
+        setTheme("carbonfox-dark");
     });
 </script>
 
-
+<link
+    rel="stylesheet"
+    href="https://www.nerdfonts.com/assets/css/webfont.css"
+/>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { invoke } from "@tauri-apps/api/core";
+    import { invoke } from "@tauri-apps/api/core";
     let tools = [
         { id: "text", name: "Text Tools", icon: "nf-seti-text" },
         { id: "pdf", name: "PDF Tools", icon: "nf-seti-pdf" },
@@ -19,18 +19,18 @@ import { invoke } from "@tauri-apps/api/core";
         { id: "soon", name: "Coming Soon", icon: "nf-fae-comet" },
     ];
 
-        async function create_window(id: String, title: String) {
-        await invoke("create_new_window", { id: id, title: title});
+    async function create_window(id: String, title: String) {
+        await invoke("create_new_window", { id: id, title: title });
     }
-
 </script>
 
 <main class="main">
+
     <div class="tools-box">
         {#each tools as tool}
             <button
                 class="tools-buttons"
-                on:click={() => create_window(tool.id,tool.name)}
+                on:click={() => create_window(tool.id, tool.name)}
             >
                 <i class="{tool.icon} tool-icon"></i>
 
